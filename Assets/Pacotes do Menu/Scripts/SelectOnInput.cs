f0d6db67d9e44bca94a3744df59ede0fa662221d
Ellen Chalegre <ellen_chalegre@hotmail.com>
2019-09-09 10:23:53 -0300

    Finalização do Menu
    Adição de navegação pelo teclado independente de outros periféricos. Adição do painel de seleção de tema de navegação.

---
+++ Assets/Pacotes do Menu/Scripts/SelectOnInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour{
+
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+    // É chamado no primeiro update de frame.
+    void Start(){
+        
+    }
+
+    // É chamado uma vez por frame.
+    void Update(){
+        if(Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false){
+            eventSystem.SetSelectedGameObject(selectedObject);
+            buttonSelected = true;
+        }
+    }
+
+    private void OnDisable(){
+        buttonSelected = false;
+    }
+}
