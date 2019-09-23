fdef7a334df650995bda693d461d36ff481a9b6a
Ellen Chalegre <ellen_chalegre@hotmail.com>
2019-09-23 10:25:54 -0300

    Adicionado recurso WEB.
    Adicionado o botão "Curiosidades" aos modelos 3D, que abre uma página WEB sobre o assunto.

---
+++ Assets/Pacotes do Menu/Scripts/WebURL.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebURL : MonoBehaviour
+{
+   
+    public void ButtonWeb(string address){
+        Application.OpenURL(address);
+    }
+
+}
